338e007064753523e2c9cb739e5c5f7300cb15c3
anfractuosity <anfractuosity@anfractuosity.com>
2019-08-02 20:41:08 +0100

    stereo connector

--- case.py
+++ case.py
@@ -6,6 +6,9 @@ doc = FreeCAD.newDocument()
 myPart = doc.addObject("Part::Feature","myPartName")
 
 incut = 4	    # padding which will be used to add buttons etc.
+
+batteryheight = 40
+
 drivelength = 150
 drivewidth = 102
 driveheight = 25    # floppy drive height in mm
@@ -16,7 +19,7 @@ A7 = 41.28
 A6 = 44.45 
 A13 = 76.20   
 
-cube = Part.makeBox(drivelength,drivewidth,driveheight+incut)
+cube = Part.makeBox(drivelength+batteryheight,drivewidth,driveheight+incut)
 cube = cube.makeThickness([ cube.Faces[0] ],wallthickness,0.1)
 
 logo = Part.makeBox(drivelength,drivewidth,incut)
@@ -34,6 +37,37 @@ doc.removeObject(txt.Label)
 myPart.Shape = cube
 
 doc.addObject('PartDesign::Body','Body001')
+
+SocketSketch = doc.Body001.newObject('Sketcher::SketchObject','SketchHoleSocket') 
+SocketSketch.Support = (myPart,["Face9"])
+SocketSketch.MapMode = 'FlatFace'
+doc.recompute()
+
+SocketSketch.addGeometry(Part.Circle(App.Vector(drivelength + (batteryheight/2.0),15,0), App.Vector(0,0,1), 7),False)
+
+pocket = doc.Body001.newObject("PartDesign::Pocket","Pocket")
+pocket.Profile = App.activeDocument().SketchHoleSocket
+pocket.Length = 5.0
+
+Gui.activeDocument().hide("SketchHoleSocket")
+
+"""
+SHole = doc.Body001.newObject("PartDesign::Hole", "SHole")
+SHole.Profile = SocketSketch
+SHole.Depth = 3
+SHole.Threaded = 0
+SHole.ThreadType = 1
+SHole.ThreadSize = 7
+SHole.DrillPointAngle = 118.000000
+
+Gui.activeDocument().hide("myPartName")
+Gui.activeDocument().setEdit('SHole', 0)
+
+"""
+
+doc.recompute()
+
+
 
 HoleSketch = doc.Body001.newObject('Sketcher::SketchObject','SketchHole') 
 HoleSketch.Support = (myPart,["Face9"])
@@ -59,7 +93,6 @@ Gui.activeDocument().hide("myPartName")
 Gui.activeDocument().setEdit('Hole', 0)
 
 doc.recompute()
-
 HoleSketch2 = doc.Body001.newObject('Sketcher::SketchObject','SketchHole2') 
 HoleSketch2.Support = (myPart,["Face13"])
 HoleSketch2.MapMode = 'FlatFace'
